74920735453ffb1fa41734ba52e07d777f2174c5
Joshua Brunke <seafortghost@gmail.com>
2025-04-08 18:40:15 +0200

    Added rendering function for the first 20 pokemon, added error card

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
-function init() {}
+function init() {
+  getDexData();
+}
 
 async function getData(soughtPokemon) {
   try {
     let url = `https://pokeapi.co/api/v2/pokemon/${soughtPokemon}`;
     let response = await fetch(url);
-    let currentSearch = await response.json();
-    console.log(currentSearch);
-    renderSoughtPokemon(currentSearch);
+    let pokemon = await response.json();
+    console.log(pokemon);
+    renderSoughtPokemon(pokemon);
   } catch (error) {
     console.error("Error fetching data:", error);
+    renderSearchErrorCard();
   }
+  document.getElementById("search-field").value = "";
 }
 
-function renderSoughtPokemon(currentSearch) {
+function renderSoughtPokemon(pokemon) {
   let contentContainer = document.getElementById("search-result");
-  let currentObject = currentSearch.synsets;
   contentContainer.innerHTML = "";
-  contentContainer.innerHTML = /*html*/ `
-        <div class="found-pokemon">
-            <h3>You found:</h3>
-            <div class= "card dexCard">
-                <h3 class="capitalize">${currentSearch.name}</h3>
-                <img src="${currentSearch.sprites.front_default}" alt="${currentSearch.name}">
-                <p>Height: ${currentSearch.height}</p>
-                <p>Weight: ${currentSearch.weight}</p>
-            </div>
-        </div>`;
+  contentContainer.innerHTML = dexCardTemplate(pokemon);
 }
 
 function sendSearch() {
@@ -38,4 +32,66 @@ function renderLoadingScreen() {
   contentContainer.innerHTML = /*html*/ `
         <div class="loading">Loading...</div>
     `;
+  contentContainer.innerHTML = "";
+}
+
+async function getDexData() {
+  try {
+    renderLoadingScreen();
+    let url = `https://pokeapi.co/api/v2/pokemon?limit=100&offset=0`;
+    let response = await fetch(url);
+    let responseToJson = await response.json();
+    console.log(responseToJson);
+    renderCollection(responseToJson);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+async function renderCollection(pokeCollection) {
+  let contentContainer = document.getElementById("pokedex");
+  contentContainer.innerHTML = "";
+
+  for (let i = 0; i < 20; i++) {
+    let pokemonEntry = pokeCollection.results[i];
+
+    try {
+      let response = await fetch(pokemonEntry.url);
+      let pokemon = await response.json();
+      contentContainer.innerHTML += dexCardTemplate(pokemon);
+    } catch (error) {
+      console.error(`Error fetching data for ${pokemonEntry.name}:`, error);
+      renderDexErrorCard();
+    }
+  }
+}
+
+function dexCardTemplate(pokemon) {
+  return /*html*/ `
+           <div class="card dex-card" id="${pokemon.id}">
+               <h3 class="capitalize">${pokemon.name}</h3>
+               <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+               <p>Height: ${pokemon.height}</p>
+               <p>Weight: ${pokemon.weight}</p>
+           </div>`;
+}
+
+function renderSearchErrorCard() {
+  let contentContainer = document.getElementById("search-result");
+  contentContainer.innerHTML = "";
+  contentContainer.innerHTML += errorCardTemplate();
+}
+
+function renderDexErrorCard() {
+  let contentContainer = document.getElementById("pokedex");
+  contentContainer.innerHTML += errorCardTemplate();
+}
+
+function errorCardTemplate() {
+  return /*html*/ `
+      <div class="card dex-card error">
+        <h3>"Oh no!"</h3>
+        <img class="error-img" src="./assets/img/questionmark.png" alt="Question mark">
+        <p>This Pokémon is hiding and won't come out.</p>
+      </div>`;
 }
