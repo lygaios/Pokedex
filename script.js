817e0861b5670cb6f028e79ff7f68bcd2291cea4
Joshua Brunke <seafortghost@gmail.com>
2025-04-08 09:19:18 +0200

    added try/catch to search function

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-function init() {
-
-}
-
+function init() {}
 
 async function getData(soughtPokemon) {
-
-    let url = `https://pokeapi.co/api/v2/pokemon/${soughtPokemon}`
+  try {
+    let url = `https://pokeapi.co/api/v2/pokemon/${soughtPokemon}`;
     let response = await fetch(url);
-    let currentRequest = await response.json();
-  
-    renderRequest(currentRequest);
+    let currentSearch = await response.json();
+    console.log(currentSearch);
+  } catch (error) {
+    console.error("Error fetching data:", error);
   }
-  
-  function renderRequest(currentRequest) {
-    let contentContainer = document.getElementById("search-result");
-    let currentObject = currentRequest.synsets;
-    contentContainer.innerHTML = "";
-  
-    for (let i = 0; i < currentObject.length; i++) {
-      const element = currentObject[i];
-      console.log(element);
-      let printableResult = element.terms;
-  
-      for (let j = 0; j < printableResult.length; j++) {
-        const element = printableResult[j].term;
-        contentContainer.innerHTML += /*html*/`
+  renderSoughtPokemon(currentSearch);
+}
+
+function renderSoughtPokemon(currentSearch) {
+  let contentContainer = document.getElementById("search-result");
+  let currentObject = currentSearch.synsets;
+  contentContainer.innerHTML = "";
+
+  for (let i = 0; i < currentObject.length; i++) {
+    const element = currentObject[i];
+    console.log(element);
+    let printableResult = element.terms;
+
+    for (let j = 0; j < printableResult.length; j++) {
+      const element = printableResult[j].term;
+      contentContainer.innerHTML += /*html*/ `
         <div>${element}</div>
-      `
-      }
+      `;
     }
   }
-  
-  function sendSearch(){
-    let soughtPokemon = document.getElementById("search-field").value;
-    getData(soughtPokemon);
-  }
+}
+
+function sendSearch() {
+  let soughtPokemon = document.getElementById("search-field").value;
+  getData(soughtPokemon);
+}
