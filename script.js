87c78c8dda330199c72cb02a35e34c99542d10a9
Joshua Brunke <seafortghost@gmail.com>
2025-04-08 10:28:42 +0200

    Search function functional (with issues)

--- script.js
+++ script.js
@@ -6,28 +6,28 @@ async function getData(soughtPokemon) {
     let response = await fetch(url);
     let currentSearch = await response.json();
     console.log(currentSearch);
+    renderSoughtPokemon(currentSearch);
   } catch (error) {
     console.error("Error fetching data:", error);
   }
-  renderSoughtPokemon(currentSearch);
 }
 
 function renderSoughtPokemon(currentSearch) {
   let contentContainer = document.getElementById("search-result");
   let currentObject = currentSearch.synsets;
   contentContainer.innerHTML = "";
-
+    contentContainer.innerHTML = /*html*/ `
+        <div class="found-pokemon">
+            <h3>You found:</h3>
+            <h3>${currentSearch.name}</h3>
+            <img src="${currentSearch.sprites.front_default}" alt="${currentSearch.name}">
+            <p>Height: ${currentSearch.height}</p>
+            <p>Weight: ${currentSearch.weight}</p>
+        </div>`;
   for (let i = 0; i < currentObject.length; i++) {
-    const element = currentObject[i];
-    console.log(element);
-    let printableResult = element.terms;
-
-    for (let j = 0; j < printableResult.length; j++) {
-      const element = printableResult[j].term;
-      contentContainer.innerHTML += /*html*/ `
-        <div>${element}</div>
-      `;
-    }
+    let foundPokemon = currentObject[i].name;
+    console.log(foundPokemon);
+    let printableResult = foundPokemon.name;
   }
 }
 
@@ -35,3 +35,31 @@ function sendSearch() {
   let soughtPokemon = document.getElementById("search-field").value;
   getData(soughtPokemon);
 }
+
+function renderLoadingScreen() {
+    let contentContainer = document.getElementById("pokedex");
+    contentContainer.innerHTML = /*html*/ `
+        <div class="loading">Loading...</div>
+    `;
+}
+
+function renderPokegallery() {
+    let contentContainer = document.getElementById("pokedex");
+    contentContainer.innerHTML = "";
+    contentContainer.innerHTML = /*html*/`
+        
+    `
+}
+
+function render() {
+    for (let i = 1; i < photos.length + 1; i++) {
+      document.getElementById("gallery").innerHTML += renderGallery(i);
+    }
+  }
+  
+  function renderGallery(i) {
+    return `
+   <div class="photosquare">
+      <img class="photopreview" onclick="showDetail(${i})" src="./img/${i}.jpg" alt"photo ${i}">
+    </div>`;
+  }
