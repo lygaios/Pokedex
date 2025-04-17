4a3810a650dd53b1d63266e316caf43f73cc4412
Joshua Brunke <seafortghost@gmail.com>
2025-04-17 17:42:42 +0200

    Added jsdoc comment to utils.js function

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,3 +1,11 @@
+/**
+ * Utility functions for the website
+ * @param {string} url - The URL to check
+ * @returns {boolean} - Returns true if the URL is external, false otherwise
+ * This function sets the target and rel attributes for external links
+ * to open in a new tab and prevent security vulnerabilities.
+ */
+
 function addAttributesToExternalLinks() {
   const externalLinks = document.querySelectorAll("a.external-link");
 
